46d615c8b173fa5246e6984720e50edad782cccf
renantescaro <renantescaro@yahoo.com.br>
2021-01-17 14:48:24 -0300

    alterando rota mostrar imagens

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify, send_from_directory, send_file, render_template
+from flask import Flask, jsonify, url_for, send_file
 from flask_caching import Cache
 from controller.clima_ctrl import ClimaCtrl
 from flask_cors import CORS
 
-app = Flask('api_clima', static_url_path='/static/', static_folder='/static/')
+app = Flask('api_clima', static_url_path='/static', static_folder='static')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 app.config['CACHE_TYPE'] = 'simple'
 app.cache = Cache(app)
@@ -22,11 +22,9 @@ def por_cidade(cidade, pais):
     return jsonify(climaCtrl.por_cidade(cidade, pais))
 
 #@app.cache.cached(timeout=300)
-@app.route('/imagem/<imagem>', methods=['GET'])
+@app.route('/img/<imagem>', methods=['GET'])
 def imagem(imagem):
-    #return send_file('static/imagens/'+str(imagem), mimetype='image/PNG')
-    #return send_from_directory('static/imagens/', imagem)
-    return render_template('img.html',imagem=imagem)
+    return send_file('static/images/'+str(imagem), mimetype='image/PNG')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host='0.0.0.0')
