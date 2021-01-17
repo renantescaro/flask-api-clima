45ab359c5afa0ce1b69d83a2288bb1393d5524ae
renantescaro <renantescaro@yahoo.com.br>
2021-01-16 22:26:52 -0300

    criando metodo para retorno de imagens

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory, send_file, render_template
 from flask_caching import Cache
 from controller.clima_ctrl import ClimaCtrl
 from flask_cors import CORS
 
-app = Flask(__name__, static_url_path='/assets/imagens/', static_folder='/assets/imagens/')
+app = Flask('api_clima', static_url_path='/static/', static_folder='/static/')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 app.config['CACHE_TYPE'] = 'simple'
 app.cache = Cache(app)
@@ -21,4 +21,12 @@ def por_latitude_longitude(lat, lon):
 def por_cidade(cidade, pais):
     return jsonify(climaCtrl.por_cidade(cidade, pais))
 
-app.run(port=5000, debug=True, host='0.0.0.0')
+#@app.cache.cached(timeout=300)
+@app.route('/imagem/<imagem>', methods=['GET'])
+def imagem(imagem):
+    #return send_file('static/imagens/'+str(imagem), mimetype='image/PNG')
+    #return send_from_directory('static/imagens/', imagem)
+    return render_template('img.html',imagem=imagem)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True, host='0.0.0.0')
