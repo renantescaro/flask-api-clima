7154d609e2e2829c18dd4b8b51619ccc4d68878e
renantescaro <renantescaro@yahoo.com.br>
2021-01-17 15:49:14 -0300

    adicionando teste verificação json

--- tests/index_test.py
+++ tests/index_test.py
@@ -16,5 +16,15 @@ class IndexTest(unittest.TestCase):
     def test_content_type(self):
         self.assertIn('application/json', self.response.content_type, msg='Erro content type')
 
+    def test_content(self):
+            response_str = self.response.data.decode('utf-8')
+            self.assertIn('"chuva":', str(response_str), msg='Erro retorno sem chave "chuva"')
+            self.assertIn('"data":', str(response_str), msg='Erro retorno sem chave "data"')
+            self.assertIn('"endereco":', str(response_str), msg='Erro retorno sem chave "endereco"')
+            self.assertIn('"temperatura":', str(response_str), msg='Erro retorno sem chave "temperatura"')
+            self.assertIn('"umidade":', str(response_str), msg='Erro retorno sem chave "umidade"')
+            self.assertIn('"urlImagem":', str(response_str), msg='Erro retorno sem chave "urlImagem"')
+            self.assertIn('"vento":', str(response_str), msg='Erro retorno sem chave "vento"')
+
 if __name__ == '__main__':
     unittest.main()
